c1934ec1870125162f5164f4b0f53dc712807137
Yogi Sugandi <yogi.sugandi@gmail.com>
2025-12-14 15:18:18 +0700

    perbaikan gambar

--- forms/kontak.php
+++ forms/kontak.php
@@ -29,7 +29,7 @@ $msg .= "Browser info : " . $_SERVER["HTTP_USER_AGENT"] . "\r\n";
 $msg .= "User come from : " . $_SERVER["SERVER_NAME"] . "\r\n";
 $msg .= "Template Name : Rajain Food";
 
-$recipient = "yogi.sugandi@gmail.com"; // Change the recipient email adress to your adrees  
+$recipient = "info@rajainfood.com"; // Change the recipient email adress to your adrees  
 $sujet =  "Sender information";
 $mailheaders = "From: $email\r\nReply-To: $email\r\nReturn-Path: $email\r\n";
 
